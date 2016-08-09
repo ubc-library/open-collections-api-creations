2a7527ca4a428c0f7b1afd82b2775672d13aa0c2
Schuyler <schuyler.lindberg@ubc.ca>
2016-08-09 12:29:24 -0700

    user feedback added to get_search_metadata.py

--- schuyberg/oc-api-utilities/get_search_metadata.py
+++ schuyberg/oc-api-utilities/get_search_metadata.py
@@ -2,7 +2,7 @@
 #  execute Open Collections search and write metadata to a json file
 #  @schuyberg 2016
 
-import json, requests
+import json, requests, sys
 
 ###########################
 # CONFIG
@@ -14,12 +14,15 @@ ocApiUrl = 'https://oc-index.library.ubc.ca' # APPY URL
 apiKey = 'f8fe4ee14c553f163d8915db3af4fb4c554dbbf7e19f2e38aedfa95ab258f806'
 
 # edit these to change the results
-search_index = '24'
-query = 'program: (business administration)'
+search_index = 'oc'
+query = 'program: ("music")'
 source_fields = ['title','_id', 'ubc.internal.repo.handle','degree', 'affiliation', 'program']
 
 #edit these to set the output file(s)
-filename = 'biz_admin'
+filename = 'music'
+
+# run with test as true for testing, false (or no argument) to write to file
+test = False
 
 
 ###########################
@@ -55,10 +58,31 @@ def doSearch():
     #post the search
     searchUrl = ocApiUrl+'/search?apiKey='+apiKey
     apiResponse = requests.post(searchUrl, json=search).json()
+    global total
     total = apiResponse['data']['data']['hits']['total']
     # print('RESPONSE:')
     # print(json.dumps(apiResponse, indent=4, sort_keys=True))
     return apiResponse['data']['data']['hits']['hits']
+
+# user feedback (progress)
+items_processed = 0
+def progress():
+    global items_processed, total
+    items_processed += 1
+    sys.stdout.write('---> ' + str(items_processed) + ' of ' + str(total) + ' items read' + '\r')
+    sys.stdout.flush(); 
+
+# parse data
+#### edit this to modify ouput #####
+def parseResults(results, resultAction):
+    for idx,r in enumerate(results):
+        d = dict()
+        d = r['_source']
+        d['id'] = r['_id']
+        d['text_snippet'] = r['fields']['ubc.transcript'][0][:5000]
+        # writeToFile()
+        resultAction(jsonfile, d)
+        progress()
 
 # output
 jsonfile = filename + '.json'
@@ -76,23 +100,10 @@ def endFile(file):
     with open(file, 'a') as output:
         output.write(']')
 
-# for testing
+    # for testing
 def printOutput(file, data):
     print('destination: ' + file)
     print(json.dumps(data, indent=4, sort_keys=True))
-
-
-# parse data
-### edit this to modify ouput
-def parseResults(results, resultAction):
-    for idx,r in enumerate(results):
-        d = dict()
-        d = r['_source']
-        d['id'] = r['_id']
-        d['text_snippet'] = r['fields']['ubc.transcript'][0][:5000]
-
-        # writeToFile()
-        resultAction(jsonfile, d)
 
 # loop through all results
 def getAllResults(resultAction):
@@ -105,18 +116,16 @@ def getAllResults(resultAction):
         offset = offset + size
         getAllResults(resultAction)
 
-
 #execute
 def execute(test=False):
+    print 'working...'
     if test:
         return getAllResults(printOutput)
-
     createFile(jsonfile)
     getAllResults(writeToFile)
     endFile(jsonfile)
+    print 'data extraction complete: output written to ' + jsonfile
 
-# run with test as argument for testing, no argument to write to file
-test = True
 execute(test)
 
 
